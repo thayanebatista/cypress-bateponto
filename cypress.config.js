fdd721c947954129723da350c4e770ec21379b6c
Thayane Batista <thayane@jera.com.br>
2023-06-14 23:22:43 -0400

    [UPDATE/WIP] login and path buttons

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
+const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions')
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  component: {
+    viewportWidth: 500,
+    viewportHeight: 500,
+  },
+  waitForAnimations: true,
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config = cypressBrowserPermissionsPlugin(on, config)
     },
   },
+  env: {
+    'browserPermissions': {
+      'geolocation': 'allow',
+    }
+  }
 });
--- cypress/e2e/bateponto.cy.js
+++ cypress/e2e/bateponto.cy.js
@@ -1,5 +1,29 @@
-describe('accessing pontomais website and login', () => {
-  it('login', () => {
-    cy.visit('https://app2.pontomais.com.br/registrar-ponto')
-  })
-})
+const { isPermissionAllowed } = require("cypress-browser-permissions");
+
+describe("accessing pontomais website and login", () => {
+  const username = "thayane@jera.com.br";
+  const password = "********";
+
+  it("user can login succefully", () => {
+    cy.visit("https://app2.pontomais.com.br/registrar-ponto");
+
+    cy.get(
+      ":nth-child(1) > pm-form > form.ng-untouched > :nth-child(1) > .row > :nth-child(1) > .ng-untouched > .form-group > .pm-input-content > .pm-input > .inside-icon > .form-control"
+    ).type(username);
+
+    cy.get(
+      ":nth-child(1) > pm-form > form.ng-dirty > :nth-child(1) > .row > .password-field > .ng-untouched > .form-group > .pm-input-content > .pm-input > .inside-icon > .form-control"
+    ).type(password);
+
+    cy.get(
+      ":nth-child(4) > :nth-child(1) > .pm-spining-btn > .pm-button"
+    ).click();
+
+    cy.log(isPermissionAllowed("geolocation"));
+    expect(isPermissionAllowed("geolocation")).to.be.true;
+
+    if (isPermissionAllowed("geolocation")) {
+      cy.get(".mt-1 > .pm-button").click();
+    }
+  });
+});
